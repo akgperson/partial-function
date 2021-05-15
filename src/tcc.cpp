812d882a6964bcdda600f81c1f28178655ead62a
Allison <ahumanlamp@gmail.com>
2021-05-15 14:19:26 -0700

    added tcc buildup cache... have both caches being built up

--- src/tcc.cpp
+++ src/tcc.cpp
@@ -18,8 +18,48 @@ smt::TreeWalkerStepResult TCCGenerator::visit_term(smt::Term &formula, smt::Term
   Op op = t->get_op();
 
   if (!op.is_null()){
-    //TODO add for tcc buildup
-    if (op.prim_op == Mod || op.prim_op == Mod || op.prim_op == Mod) {
+
+    TermVec cached_children;
+    TermVec cached_tcc;
+
+    for (auto c : t){
+      cached_children.push_back(c);
+      cached_tcc.push_back(tcc_cache_.at(c));
+    }
+
+    if (op.prim_op == Or) {
+      Term condition = solver_->make_term(Or, solver_->make_term(And, cached_tcc[0], cached_children[0]), solver_->make_term(And, cached_tcc[1], cached_children[1]), solver_->make_term(And, cached_tcc[0], cached_tcc[1]));
+      tcc_cache_[t] = condition;
+    }
+
+    else if (op.prim_op == And) {
+      Term condition = solver_->make_term(Or, solver_->make_term(And, cached_tcc[0], solver_->make_term(Not, cached_children[0])), solver_->make_term(And, cached_tcc[1], solver_->make_term(Not, cached_children[1])), solver_->make_term(And, cached_tcc[0], cached_tcc[1]));
+      tcc_cache_[t] = condition;
+    }
+    else if (op.prim_op == Ite) {
+      Term condition = solver_->make_term(And, cached_tcc[0], solver_->make_term(Ite, cached_children[0], cached_tcc[1], cached_tcc[2]));
+      tcc_cache_[t] = condition;
+    }
+    else if (op.prim_op == Implies) {
+      Term condition = solver_->make_term(Or, solver_->make_term(And, cached_tcc[0], solver_->make_term(Not, cached_children[0])), solver_->make_term(And, cached_tcc[1], cached_children[1]), solver_->make_term(And, cached_tcc[0], cached_tcc[1]));
+      tcc_cache_[t] = condition;
+    }
+    else if (op.prim_op == Not) {
+      Term condition = cached_tcc[0];
+      tcc_cache_[t] = condition;
+    }
+//    else if (op.prim_op == Iff) {
+  //    Term subcondition1 = solver_->make_term(And, cached_children[0], cached_children[1]);
+    //  Term subcondition2 = solver_->make_term(And, solver_->make_term(Not, cached_children[0]), solver_->make_term(Not, cached_children[1]));
+      //Term subcondition3 = solver_->make_term(Or, solver_->make_term(And, cached_tcc[0], solver_->make_term(Not, cached_children[0])), solver_->make_term(And, cached_tcc[1], solver_->make_term(Not, cached_children[1])), solver_->make_term(And, cached_tcc[0], cached_tcc[1]));
+      //Term subcondition4 = solver_->make_term(Or, solver_->make_term(And, cached_tcc[0], cached_children[0]), solver_->make_term(And, cached_tcc[1], cached_children[1]), solver_->make_term(And, cached_tcc[0], cached_tcc[1]));
+      //Term condition = solver_->make_term(Or, solver_->make_term(And, subcondition1, subcondition3), solver_->make_term(And, subcondition4, subcondition2), solver_->make_term(And, subcondition3, subcondition4));
+      //tcc_cache_[t] = condition;
+    //}
+
+    //TODO add quantifiers
+
+    else if (op.prim_op == Mod || op.prim_op == Mod || op.prim_op == Mod) {
       string var_name = string("b") + to_string(b_iter);
       Term b = solver_->make_symbol(var_name, boolsort);
       b_iter++;
@@ -31,12 +71,32 @@ smt::TreeWalkerStepResult TCCGenerator::visit_term(smt::Term &formula, smt::Term
       save_in_cache(b, occ);
 
       fresh_bns = solver_->make_term(And, fresh_bns, solver_->make_term(Equal, b, bool_true_));
-//      Term condition = solver_->make_term(And, cached_tcc[0], cached_tcc[1], solver_->make_term(Distinct, cached_children[1], int_zero_));
-//      cache_[t] = condition;
+
+      Term fresh_condition = solver_->make_term(Equal, b, solver_->make_term(Distinct, cached_children[1], int_zero_));
+      Term condition = solver_->make_term(And, cached_tcc[0], cached_tcc[1], fresh_condition);
+      tcc_cache_[t] = condition;
+    }
+
+    else { //op w/o undefined behavior
+      Term condition;
+      if (cached_tcc.size() == 1) {
+        condition = cached_tcc[0];
+      }
+      else {
+        condition = solver_->make_term(And, cached_tcc);
+      }
+      tcc_cache_[t] = condition;
     }
   }
   else { //at leaf
       tcc_cache_[t] = solver_->make_term(true); //change
+  }
+
+  // add fresh bns to topmost node
+  // TODO could this have problems... will a sub node ever be the same as the top node... no, right?
+  if (formula == t){ //TODO this equality all right?
+    Term old_topmost = tcc_cache_[t];
+    tcc_cache_[t] = solver_->make_term(And, old_topmost, fresh_bns);
   }
   return TreeWalker_Continue;
 }
